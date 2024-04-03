d5878b4e6b01f3501a17ab190ee958bf3f2a795f
Alícia Ledra <alicia.ledra@gmail.com>
2024-04-02 21:24:09 -0300

    Get-Set
    Tentando iniciar Get Set

--- Aula-Herança-Randerson/Livro.java
+++ Aula-Herança-Randerson/Livro.java
@@ -1,7 +1,12 @@
 public class Livro {
-    String Nome;
-    String Autor;
-    int NumeroPaginas;
+   private String Nome;
+   private String Autor;
+   private int NumeroPaginas;
+   public String getNome();
+
+   public void setNome(String nome) {
+    this.nome = nome;
+    }
 
     public Livro (String Nome, String Autor, int NumeroPaginas) {
       this.Nome = Nome;
